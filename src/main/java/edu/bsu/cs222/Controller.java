e5775fdd46d946f7215151bd37af712f40ecfa83
Chukeze <46633640+Chukeze@users.noreply.github.com>
2020-10-30 11:50:53 -0400

    tictictoe

---
+++ src/main/java/edu/bsu/cs222/Controller.java
@@ -0,0 +1,106 @@
+package edu.bsu.cs222;
+
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.control.*;
+
+import java.util.ArrayList;
+
+public class Controller extends CalendarUI{
+
+    @FXML
+    Pane gamePane;
+
+    @FXML
+    public Label gameName;
+
+    @FXML
+    private Pane ProgressBar;
+
+    @FXML
+    private Pane ticTacToe;
+
+
+    @FXML
+    private Button startDailyChallengeButton;
+
+    @FXML
+    public Label gameNotificationLabel;
+
+    @FXML
+    private TicTacToeController ticTacToeController;
+
+    @FXML
+    private ProgressBar progressBarController;
+
+    public ArrayList<String> gameList = new ArrayList<>();//all the games to be randomize and create
+
+    private String currentGame = "Main Menu";
+    //private int[] gameChosen; fix later
+
+
+    public void initialize(){
+        addGamesToTheGameList();
+        //progressBarController.initialize();
+        startDailyChallengeButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                gameNotificationLabel.setText("");
+                startTicTacToe(0);
+            }
+        });
+    }
+    private void addGamesToTheGameList(){
+        gameList.add("Tic Tac Toe");
+        //gameList.add(ticTacToeController) fix later
+    }
+
+    /**public void randomizeGame(){//parse thru game choices and load game
+        gameList; //list of games
+    }
+    public void startChallenge(int mode){//begins daily cha
+        randomizeGame();
+        currentGame = " ";
+
+        resetGamePane();
+        resetProgressBar();
+        ticTacToeController.initialize(this,mode);
+        ticTacToe.setVisible(true);
+        gameName.setText("Tic Tac Toe");
+
+    }*/
+    public void startTicTacToe(int mode){
+        currentGame ="TicTacToe";
+        resetGamePane();
+        ticTacToeController.initialize(this, mode);
+        ticTacToe.setVisible(true);
+        gameName.setText("TicTacToe");
+    }
+
+    private void resetGamePane() {
+        Pane[] gamePane= new Pane[]{
+                ticTacToe
+        };
+        for (Pane pane : gamePane){
+            pane.setVisible(false);
+        }
+    }
+
+
+    public void restartProgressBar() {
+    }
+
+    public void notifyLoss() {
+    }
+
+    public void progressToNextDay(int i) {
+    }
+
+    public void notifyWin() {
+
+    }
+}
---
+++ src/main/java/edu/bsu/cs222/TicTacToeController.java
@@ -0,0 +1,274 @@
+package edu.bsu.cs222;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
+import java.util.ArrayList;
+
+public class TicTacToeController {
+
+    @FXML
+    GridPane board;
+
+    @FXML
+    Button cell0;
+
+    @FXML
+    Button cell1;
+
+    @FXML
+    Button cell2;
+
+    @FXML
+    Button cell3;
+
+    @FXML
+    Button cell4;
+
+    @FXML
+    Button cell5;
+
+    @FXML
+    Button cell6;
+
+    @FXML
+    Button cell7;
+
+    @FXML
+    Button cell8;
+
+    private ArrayList<Button> cellList = new ArrayList<>();
+    private int turnNumber = 0;
+    private int mode;
+    private boolean gameIsPlaying = false;
+    private TicTacToe game;
+    private Controller mainController;
+
+    public void initialize(Controller controller, int mode){
+        this.mainController = controller;
+        this.mode = mode;
+        startGame();
+    }
+    @FXML
+    private void CellZero(){
+        if (cell0.getText().equals("") && gameIsPlaying){
+            turnNumber++;
+            cell0.setText("X");
+            updateGameStatus();
+            if (turnNumber % 2 == 1){
+                computerMove();
+            }
+        }
+    }
+    @FXML
+    private void CellOne(){
+        if (cell1.getText().equals("") && gameIsPlaying){
+            turnNumber++;
+            cell1.setText("X");
+            updateGameStatus();
+            if (turnNumber % 2 == 1){
+                computerMove();
+            }
+        }
+    }
+    @FXML
+    private void CellTwo(){
+        if (cell2.getText().equals("") && gameIsPlaying){
+            turnNumber++;
+            cell2.setText("X");
+            updateGameStatus();
+            if (turnNumber % 2 == 1){
+                computerMove();
+            }
+        }
+    }
+    @FXML
+    private void CellThree(){
+        if (cell3.getText().equals("") && gameIsPlaying){
+            turnNumber++;
+            cell3.setText("X");
+            updateGameStatus();
+            if (turnNumber % 2 == 1){
+                computerMove();
+            }
+        }
+    }
+    @FXML
+    private void CellFour(){
+        if (cell4.getText().equals("") && gameIsPlaying){
+            turnNumber++;
+            cell4.setText("X");
+            updateGameStatus();
+            if (turnNumber % 2 == 1){
+                computerMove();
+            }
+        }
+    }
+    @FXML
+    private void CellFive(){
+        if (cell5.getText().equals("") && gameIsPlaying){
+            turnNumber++;
+            cell5.setText("X");
+            updateGameStatus();
+            if (turnNumber % 2 == 1){
+                computerMove();
+            }
+        }
+    }
+    @FXML
+    private void CellSix(){
+        if (cell6.getText().equals("") && gameIsPlaying){
+            turnNumber++;
+            cell6.setText("X");
+            updateGameStatus();
+            if (turnNumber % 2 == 1){
+                computerMove();
+            }
+        }
+    }
+    @FXML
+    private void CellSeven(){
+        if (cell7.getText().equals("") && gameIsPlaying){
+            turnNumber++;
+            cell7.setText("X");
+            updateGameStatus();
+            if (turnNumber % 2 == 1){
+                computerMove();
+            }
+        }
+    }
+    @FXML
+    private void CellEight(){
+        if (cell8.getText().equals("") && gameIsPlaying){
+            turnNumber++;
+            cell8.setText("X");
+            updateGameStatus();
+            if (turnNumber % 2 == 1){
+                computerMove();
+            }
+        }
+    }
+    private void startGame(){
+        gameIsPlaying= true;
+        game = new TicTacToe();
+        turnNumber = 0;
+        cellList = new ArrayList<>();
+        fillCellList();
+        resetBoard();
+        mainController.gameNotificationLabel.setText("Win to see the next Day");
+    }
+
+    private void fillCellList() {
+        cellList.add(cell0);
+        cellList.add(cell1);
+        cellList.add(cell2);
+        cellList.add(cell3);
+        cellList.add(cell4);
+        cellList.add(cell5);
+        cellList.add(cell6);
+        cellList.add(cell7);
+        cellList.add(cell8);
+    }
+    private void computerMove() {
+        ArrayList<Integer> openCells = game.gameStatus.findOpenCells();
+        turnNumber++;
+        if(openCells.size() > 0 && gameIsPlaying) {
+            int cell = game.computerTurn();
+            if(cell == -1) {
+                int randomInt = (int) (Math.random() * openCells.size());
+                Button chosenButton = cellList.get(openCells.get(randomInt));
+                chosenButton.setText("O");
+            }
+            else{
+                Button chosenButton = cellList.get(cell);
+                chosenButton.setText("O");
+            }
+            updateGameStatus();
+        }
+    }
+
+    private void updateGameStatus() {
+        game.gameStatus.addMove(0,PlayerNumber(cell0.getText()));
+        game.gameStatus.addMove(0,PlayerNumber(cell1.getText()));
+        game.gameStatus.addMove(0,PlayerNumber(cell2.getText()));
+        game.gameStatus.addMove(0,PlayerNumber(cell3.getText()));
+        game.gameStatus.addMove(0,PlayerNumber(cell4.getText()));
+        game.gameStatus.addMove(0,PlayerNumber(cell5.getText()));
+        game.gameStatus.addMove(0,PlayerNumber(cell6.getText()));
+        game.gameStatus.addMove(0,PlayerNumber(cell7.getText()));
+        game.gameStatus.addMove(0,PlayerNumber(cell8.getText()));
+        if (game.gameStatus.checkBoard() !=0 || turnNumber == 9){
+            String winner = convertPlayerNumberToString(game.gameStatus.checkBoard());
+            gameIsPlaying = false;
+            if (winner != null){
+                if (winner != null){
+                    if (winner.equals("Player")){
+                        declareVictory();
+                    }else {
+                        declareLoss();
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    private void declareLoss() {
+        mainController.notifyLoss();
+        restartGame();
+        if (mode == 1){
+            mainController.restartProgressBar();
+        }
+    }
+
+    private void declareVictory() {
+        mainController.notifyWin();
+        if (mode == 0){
+            mainController.progressToNextDay(0);
+        }else if(mode == 1){
+            restartGame();
+            mainController.restartProgressBar();
+        }
+    }
+
+    private void restartGame() {
+        resetBoard();
+        startGame();
+        game.gameStatus.resetGameGrid();
+    }
+
+    private void resetBoard() {
+        for(Button cell : cellList){
+            cell.setText("");
+        }
+    }
+
+    private int PlayerNumber(String text) {
+        switch (text) {
+            case "":
+                return 0;
+
+            case "X":
+                return 1;
+
+            case "O":
+                return 2;
+        }
+        return 0;
+    }
+
+    private String convertPlayerNumberToString(int playerId) {
+        if(playerId == 0) {
+            return "Nobody";
+        }
+        else if(playerId == 1){
+            return "Player";
+        }
+        else if(playerId == 2){
+            return "Computer";
+        }
+        return null;
+    }
+}
