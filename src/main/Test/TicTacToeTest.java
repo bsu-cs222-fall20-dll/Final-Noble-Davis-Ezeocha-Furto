93816b7aba23d3ba24cdc0d144ff3d319d5c972a
Chukeze <46633640+Chukeze@users.noreply.github.com>
2020-11-12 15:12:09 -0500

    tictictoe

--- src/main/Test/TicTacToeTest.java
+++ src/main/Test/TicTacToeTest.java
@@ -1,8 +1,15 @@
+import edu.bsu.cs222.TicTacToe;
 import org.junit.Test;
 public class TicTacToeTest {
 
     @Test
     public void alwaysTrue(){
         assertTrue(true);
+    }
+    @Test
+    public void cellsAre(){
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.playerturn(5);
+        assertEquals(ticTacToe.gameStatus.cells[5],1);
     }
 }
