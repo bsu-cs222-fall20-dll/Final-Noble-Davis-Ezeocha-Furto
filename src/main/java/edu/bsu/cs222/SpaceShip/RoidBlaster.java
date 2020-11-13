6126ea2671ee38f82aa9e6cb6fe52129a7015bea
Chukeze <46633640+Chukeze@users.noreply.github.com>
2020-11-12 20:58:00 -0500

    tictictoe

--- src/main/java/edu/bsu/cs222/SpaceShip/RoidBlaster.java
+++ src/main/java/edu/bsu/cs222/SpaceShip/RoidBlaster.java
@@ -1,4 +1,29 @@
 package edu.bsu.cs222.SpaceShip;
 
-public class RoidBlaster {
+public class RoidBlaster extends Weapons {
+
+    @Override
+    public void fireRate(int x) {
+        super.fireRate(100);
+    }
+
+    @Override
+    public void shoot(boolean fire) {
+        super.shoot(true);
+    }
+
+    @Override
+    public void reloadSpeed(int y) {
+        super.reloadSpeed(10);
+    }
+
+    @Override
+    public void ammoCapacity(int capacity) {
+        super.ammoCapacity(150);
+    }
+
+    @Override
+    public void ammoColor() {
+
+    }
 }
