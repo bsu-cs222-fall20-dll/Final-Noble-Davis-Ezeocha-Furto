fbed62a3eb9fe130e11f22deb9511f0523feb45b
Chukeze <46633640+Chukeze@users.noreply.github.com>
2020-11-12 20:29:36 -0500

    tictictoe

--- src/main/java/edu/bsu/cs222/SpaceShip/Space.java
+++ src/main/java/edu/bsu/cs222/SpaceShip/Space.java
@@ -1,4 +1,22 @@
 package edu.bsu.cs222.SpaceShip;
 
 public class Space {
+    Asteroid asteroid;
+    SpaceShip spaceShip;
+
+    public Space(){//set color of the black space we are acustom to
+
+    }
+    public Asteroid getAsteroid(){
+        return asteroid;
+    }
+    public void setAsteroid(Asteroid asteroid){
+        this.asteroid = asteroid;
+    }
+    public SpaceShip getSpaceShip(){
+        return spaceShip;
+    }
+    public void setSpaceShip(SpaceShip spaceShip){
+        this.spaceShip = spaceShip;
+    }
 }
