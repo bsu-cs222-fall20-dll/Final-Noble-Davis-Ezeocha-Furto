7027aaef37bb2aa3e3f4eea8e0c438ede0e0d8e2
Chukeze <46633640+Chukeze@users.noreply.github.com>
2020-11-16 18:07:25 -0500

    tictictoe

--- src/main/java/edu/bsu/cs222/SpaceShip/Asteroid.java
+++ src/main/java/edu/bsu/cs222/SpaceShip/Asteroid.java
@@ -1,7 +1,11 @@
 package edu.bsu.cs222.SpaceShip;
 
-public class Asteroid implements moveable, drawable{
+import javafx.scene.image.Image;
 
+import java.util.Random;
+
+public class Asteroid implements moveable, drawable{
+    //implementing interefaces
     @Override
     public void move(int speed, int direction) {
 
@@ -14,4 +18,25 @@ public class Asteroid implements moveable, drawable{
     public void draw() {
 
     }
+    //asteroid Specific
+    private static final Random asteroids = new Random();
+    private static final int asteroidSize =30;
+    static final Image aesteroidImage = new Image("C:\\Users\\enile\\IdeaProjects\\Final-Noble-Davis-Ezeocha-Furto\\src\\main\\resources\\SpaceWars\\Asteroids.png");
+    static final Image asteroidExplosion = new Image("C:\\Users\\enile\\IdeaProjects\\Final-Noble-Davis-Ezeocha-Furto\\src\\main\\resources\\SpaceWars\\AesteroidExplosion.png");
+    static final int explosionWidth= 100;
+    static final int explosionHeight = 100;
+    static final int explosionRows = 3;
+    static final int explosionCol= 3;
+    static final int explosionSteps= 15;
+
+    static final  Image fallingAsteroid[] = {
+            aesteroidImage,
+            aesteroidImage,
+            aesteroidImage,
+            aesteroidImage,
+    };
+    final int maxFallingAsteroidAtOneTime = 30, maxShots= maxFallingAsteroidAtOneTime * 2;
+
+
+
 }
--- src/main/java/edu/bsu/cs222/SpaceShip/SpaceShipController.java
+++ src/main/java/edu/bsu/cs222/SpaceShip/SpaceShipController.java
@@ -1,4 +1,18 @@
 package edu.bsu.cs222.SpaceShip;
 
+import edu.bsu.cs222.Controller;
+import edu.bsu.cs222.ProgressBarController;
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
 public class SpaceShipController {
+    @FXML
+    Pane space;
+    private boolean gameIsCurrentlyBeingPlayed = false;
+    private SpaceWars game;
+    private Controller mainController;
+    final ProgressBarController progressBarController = new ProgressBarController();
+
+
+
 }
