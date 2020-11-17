998f809fa17f43259b3e2f43c4b1677659193746
Chukeze <46633640+Chukeze@users.noreply.github.com>
2020-11-17 16:43:04 -0500

    tictictoe

--- src/main/java/edu/bsu/cs222/SpaceShip/SpaceWarsGameStatus.java
+++ src/main/java/edu/bsu/cs222/SpaceShip/SpaceWarsGameStatus.java
@@ -3,5 +3,12 @@ package edu.bsu.cs222.SpaceShip;
 
 
 public class SpaceWarsGameStatus {
-
+    int asteroidDestroyed;
+    public int checkBenchmark() {
+        int benchMark= 50;
+        if(asteroidDestroyed > benchMark){
+            return 0;
+        }
+        return benchMark;
+    }
 }
