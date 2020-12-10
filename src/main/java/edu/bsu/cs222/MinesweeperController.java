628fb54ceec32c7d294988903712c016baaada20
Chukeze <46633640+Chukeze@users.noreply.github.com>
2020-12-10 01:40:38 -0500

    add an FXML Loader method -chuk

--- src/main/java/edu/bsu/cs222/MinesweeperController.java
+++ src/main/java/edu/bsu/cs222/MinesweeperController.java
@@ -1,7 +1,10 @@
 package edu.bsu.cs222;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 
 import java.util.List;
 
@@ -30,11 +33,38 @@ public class MinesweeperController {
     }
 
     private void setCellButtonHandlers() {
+        for (int i = 0; i < cells.size(); i++){
+            final Button block = cells.get(i);
+            block.setOnMouseClicked(makeEventHandlerForCell(i));
+        }
+    }
+
+    private EventHandler<? super MouseEvent> makeEventHandlerForCell(int i) {
+        EventHandler<MouseEvent> eventHandler;
+        eventHandler = new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                MouseButton button = event.getButton();
+                if (button == MouseButton.PRIMARY){
+                    sweepCell(i);
+                }else if(button == MouseButton.SECONDARY){
+                    flagged(i);
+                }
+            }
+        };
+        return eventHandler;
+    }
+
+    private void sweepCell(int i) {
+
+    }
+
+    private void flagged(int i) {
 
     }
 
     private void updateGoalLabel() {
         mainController.gameNotificationLabel.setText("Goal: Flag 9 mines (" + flagCount + "/" + bombCount + ")");
-
     }
+
 }
