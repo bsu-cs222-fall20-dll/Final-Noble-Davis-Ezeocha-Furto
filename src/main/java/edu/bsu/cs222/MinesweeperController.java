1924425e28d2cf17ca3044236c8ed66acd9aa31d
Chukeze <46633640+Chukeze@users.noreply.github.com>
2020-12-10 01:30:34 -0500

    add an FXML Loader method -chuk

--- src/main/java/edu/bsu/cs222/MinesweeperController.java
+++ src/main/java/edu/bsu/cs222/MinesweeperController.java
@@ -1,4 +1,22 @@
 package edu.bsu.cs222;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.util.List;
+
 public class MinesweeperController {
+    @FXML
+    private List<Button> cells;
+
+    private Controller mainController;
+    private Minesweeper game;
+
+    private int bombCount = 9;
+    private int flagCount = 0;
+    private int mode;
+
+    public void initialize(Controller controller, int mode){
+
+    }
 }
